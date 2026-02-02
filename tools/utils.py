614898c9956edee6b151878cc180fae5b6b20e8f
Alex Vasilyev <91796613+Guest838@users.noreply.github.com>
2026-02-02 09:54:54 +0400

    Add new functions

--- tools/utils.py
+++ tools/utils.py
@@ -231,3 +231,23 @@ def metrics_ae(pred, target, batch_img, threshold, device):
                 plt.show()
 
     return ja, prec, rec, ff1
+
+def optimizer_to(optim, device):
+    for param in optim.state.values():
+        # Not sure there are any global tensors in the state dict
+        if isinstance(param, torch.Tensor):
+            param.data = param.data.to(device)
+            if param._grad is not None:
+                param._grad.data = param._grad.data.to(device)
+        elif isinstance(param, dict):
+            for subparam in param.values():
+                if isinstance(subparam, torch.Tensor):
+                    subparam.data = subparam.data.to(device)
+                    if subparam._grad is not None:
+                        subparam._grad.data = subparam._grad.data.to(device)
+
+def plot_loss(Loss_train):
+    plt.figure(figsize=(12, 5))
+    plt.plot(range(len(Loss_train)), Loss_train, color='orange', label='train', linestyle='--')
+    plt.legend()
+    plt.show()
